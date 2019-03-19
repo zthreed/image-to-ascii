f772b8bdfdb961d7d5d86cd82a79ca8587e4c2f5
zthreed <zthreed@gmail.com>
2019-03-18 23:57:00 -0600

    Initial project creation

---
+++ Source/ImageToAscii.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ImageToAscii
+{
+    class ImageToAscii
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tests/ImageToAsciiTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace ImageToAscii.ImageToAsciiTests
+{
+    public class ImageToAsciiTests
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+    }
+}
